cf020688660dd65055bf5599e314292858e1bca4
fides <fidesosu@gmail.com>
2024-06-05 13:44:34 +0300

    Add tab plugin

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,16 @@ import { defineConfig } from 'vitepress'
 import { sidebarData } from '../dist/js/sidebar-collapsed.mjs'
 import footnote from 'markdown-it-footnote'
 
+import MarkdownIt from "markdown-it";
+import { tab } from "@mdit/plugin-tab";
+
+const mdIt = MarkdownIt().use(tab, {
+  // your options, name is required
+  name: "tabs",
+});
+
+mdIt.render("content");
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Passeli Merit API",
